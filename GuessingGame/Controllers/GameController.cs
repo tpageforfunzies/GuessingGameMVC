2f7bd1a8df0050095645e387934aaa333f6356ad
tpageforfunzies <tjpage@indiana.edu>
2017-11-13 13:59:19 -0500

    yeah

---
+++ GuessingGame/Controllers/GameController.cs
@@ -0,0 +1,24 @@
+﻿using GuessingGame.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GuessingGame.Controllers
+{
+    public class GameController : Controller
+    {
+        // GET: Game
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(GameModel model)
+        {
+            return View(model);
+        }
+    }
+}
