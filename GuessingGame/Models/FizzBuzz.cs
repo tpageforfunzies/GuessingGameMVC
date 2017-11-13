ee2221bef5fd4b196377c8ddfcfa3ff1af53a13f
tpageforfunzies <tjpage@indiana.edu>
2017-11-13 16:57:04 -0500

    add fizzbuzz model

---
+++ GuessingGame/Models/FizzBuzz.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace GuessingGame.Models
+{
+    public class FizzBuzz
+    {
+        [Required]
+        public int FizzBuzzNumber { get; set; }
+    }
+}
