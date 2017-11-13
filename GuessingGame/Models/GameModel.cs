117b10f7f8b434b2c9fde7c8b2bbb39ec5aced1a
tpageforfunzies <tjpage@indiana.edu>
2017-11-13 16:08:14 -0500

    added conditional win/loss to view

--- GuessingGame/Models/GameModel.cs
+++ GuessingGame/Models/GameModel.cs
@@ -8,15 +8,15 @@ namespace GuessingGame.Models
 {
     public class GameModel
     {
-        [Display(Name = "Enter your name")]
-        [Required(ErrorMessage = "You must enter your name")]
-        [MinLength(2, ErrorMessage = "Name is too short")]
-        [MaxLength(20, ErrorMessage = "Name is too long")]
+        [Display(Name = "What's your name?")]
+        [Required(ErrorMessage = "You must enter your name.")]
+        [MinLength(2, ErrorMessage = "Name is too short, minimum of 2 characters.")]
+        [MaxLength(20, ErrorMessage = "Name is too long, max of 20 characters.")]
         public string PlayerName { get; set; }
 
         [Display(Name = "What is your guess?")]
-        [Required(ErrorMessage = "Guess is required")]
-        [Range(1, 10, ErrorMessage = "Guess must be between 1 and 10")]
+        [Required(ErrorMessage = "Guess is required.")]
+        [Range(1, 10, ErrorMessage = "Guess must be between 1 and 10.")]
         public int Guess { get; set; }
 
     }
