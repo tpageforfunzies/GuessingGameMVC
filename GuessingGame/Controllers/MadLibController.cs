66f6a1e952e36be91f39f2dcca4bdd877c603348
tpageforfunzies <tjpage@indiana.edu>
2017-11-14 11:37:43 -0500

    added madlib project

---
+++ GuessingGame/Controllers/MadLibController.cs
@@ -0,0 +1,37 @@
+﻿using GuessingGame.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GuessingGame.Controllers
+{
+    public class MadLibController : Controller
+    {
+        // GET: Index
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //POST: Index
+        [HttpPost]
+        public ActionResult Index(MadLib model)
+        {
+            ViewBag.Story = $"The {model.Adjective1} cat was about to {model.Verb1} but suddenly a {model.Noun1} interrupted! " +
+                $"There's a giant feral {model.Noun2} coming and it will {model.Verb2} you up if you don't leave! " +
+                $"Proud of themselves, the {model.Adjective2} {model.Noun1} watched you leave and didnt see the {model.Noun2} " +
+                $"coming and promptly got wrecked.";
+            return View();
+        }
+
+        // GET: Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+
+    }
+}
---
+++ GuessingGame/Models/MadLib.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace GuessingGame.Models
+{
+    public class MadLib
+    {
+        [Display(Name = "Enter an adjective")]
+        [Required(ErrorMessage = "A word is required.")]
+        public string Adjective1 { get; set; }
+
+        [Display(Name = "Enter an adjective")]
+        [Required(ErrorMessage = "A word is required.")]
+        public string Adjective2 { get; set; }
+
+        [Display(Name = "Enter a verb")]
+        [Required(ErrorMessage = "A word is required.")]
+        public string Verb1 { get; set; }
+
+        [Display(Name = "Enter a verb")]
+        [Required(ErrorMessage = "A word is required.")]
+        public string Verb2 { get; set; }
+
+        [Display(Name = "Enter a noun")]
+        [Required(ErrorMessage = "A word is required.")]
+        public string Noun1 { get; set; }
+
+        [Display(Name = "Enter a noun")]
+        [Required(ErrorMessage = "A word is required.")]
+        public string Noun2 { get; set; }
+
+
+    }
+}
