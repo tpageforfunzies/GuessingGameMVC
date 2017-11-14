cab4d203626d43d8eeff8a9bf964f7c811dd020a
tpageforfunzies <tjpage@indiana.edu>
2017-11-13 21:49:23 -0500

    added countdown from user input number)

---
+++ GuessingGame/Controllers/ListController.cs
@@ -0,0 +1,30 @@
+﻿using GuessingGame.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GuessingGame.Controllers
+{
+    public class ListController : Controller
+    {
+        // GET: List
+        public ActionResult Index(ListModel model)
+        {
+            return View(model);
+        }
+
+        // GET: Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(ListModel model)
+        {
+            return RedirectToAction("Index", model);
+        }
+    }
+}
---
+++ GuessingGame/Models/ListModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace GuessingGame.Models
+{
+    public class ListModel
+    {
+        [Display(Name = "What number should I count down from?")]
+        [Required(ErrorMessage = "Number is required.")]
+        public int NumberToCount { get; set; }
+    }
+}
